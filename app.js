5925beb5372229a55e323f0e7a633814b3295611
Martin <martinconradpelayo@gmail.com>
2017-02-23 14:50:26 -0800

    acutallfirstbranch

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Start of the mile to kilometer program\\
+// 1 mile = 1.60934 kilometers \\
+var miles = 0;
+var kilometer = 0;
+
+function mileToKilometer {
+var calculate = Math.floor()
+}
+
+
+  return Math.floor(Math.random() * (max - min) ) + min;
