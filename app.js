2186f6160232feb2d1ef2bf655e8b4b03593cff5
mugworks <greenwood.pdx@gmail.com>
2017-02-23 15:20:01 -0800

    added two functions for inch/cm conversion

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-'use strict'
+'use strict';
 
-var inch;
-var centimeters;
 
 function inchToCm() {
-    
+  var e = document.getElementById('select');
+  var centimeters = e.options.e.selectedIndex.value;
+}
+
+function cmToInch() {
+  var e = document.getElementById('select');
+  var inch = e.options.e.selectedIndex.value;
 }
