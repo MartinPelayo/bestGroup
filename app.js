d08925887c0847e2b4daaf64224259e3814239ba
Martin <martinconradpelayo@gmail.com>
2017-02-23 16:02:50 -0800

    looking for conflict

--- app.js
+++ app.js
@@ -7,13 +7,29 @@
 
 //Start of the mile to kilometer program\\
 // 1 mile = 1.60934 kilometers \\
-var miles = 0;
-var kilometer = 0;
+//var userInput = '';
+//var userOutput = '';
 
-function mileToKilometer {
-var calculate = Math.floor()
+function mileToKilometer() {
+  var cat = documet.getElementById('someID');
+  cat.textContent = ''
+  var userInput = cat.appendChild(dog);
+
 }
-  return Math.floor(Math.random() * (max - min) ) + min;
+
+var miles = 0;
+var kilometer = 1.60934;
+
+var cat = documet.getElementById('someID');
+var dog = document.createElement('SomeElement')
+var userInput = cat.appendChild(dog);
+
+function mileToKilometer() {
+  
+  
+  var result = miles * kilometer;
+  return result;
+}
 
 
 
